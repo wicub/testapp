76df8e80e0c07d14b9d70569d43197b68f53ddb8
Charles Jia <it6780@163.com>
2013-11-06 10:11:31 +0800

    first commit

---
+++ YOWU APP/品牌_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 52; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u34'] = 'center';
---
+++ YOWU APP/首页_1_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 40; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u33'] = 'top';
